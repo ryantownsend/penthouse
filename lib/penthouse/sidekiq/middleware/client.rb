7cc9250d74f58ea86b9500e68e0aa4874e89693d
Ryan Townsend <ryan@ryantownsend.co.uk>
2016-03-17 19:18:17 +0000

    Updated Sidekiq compatibility

--- lib/penthouse/sidekiq/middleware/client.rb
+++ lib/penthouse/sidekiq/middleware/client.rb
@@ -1,8 +1,12 @@
-module Penthouse::Sidekiq::Middleware
-  class Client
-    def call(worker_class, item, queue, redis_pool=nil)
-      item['tenant'] ||= Penthouse.current_tenant
-      yield
+module Penthouse
+  module Sidekiq
+    module Middleware
+      class Client
+        def call(worker_class, item, queue, redis_pool=nil)
+          item['tenant'] ||= Penthouse.current_tenant
+          yield
+        end
+      end
     end
   end
 end
--- lib/penthouse/sidekiq/middleware/server.rb
+++ lib/penthouse/sidekiq/middleware/server.rb
@@ -1,8 +1,12 @@
-module Apartment::Sidekiq::Middleware
-  class Server
-    def call(worker_class, item, queue)
-      Penthouse.router.switch(item['tenant']) do
-        yield
+module Penthouse
+  module Sidekiq
+    module Middleware
+      class Server
+        def call(worker_class, item, queue)
+          Penthouse.switch(item['tenant']) do
+            yield
+          end
+        end
       end
     end
   end
