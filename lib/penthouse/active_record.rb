8d7b554cc468d3a82b92cd2f46afd7b1df89b4e8
Ryan Townsend <ryan@ryantownsend.co.uk>
2016-11-16 13:02:56 +0000

    Updated table_name override [2]

--- lib/penthouse/active_record.rb
+++ lib/penthouse/active_record.rb
@@ -4,17 +4,21 @@
 class ActiveRecord::Base
   DEFAULT_SCHEMA_NAME = 'public'.freeze
 
-  # override table name so it's calculated EVERY time
-  def self.table_name
-    table_name_prefix + super.gsub(/^.*\./, '')
-  end
+  class << self
+    alias_method :original_table_name, :table_name
 
-  # set a table name prefix which is dynamic based on the current schema
-  def self.table_name_prefix
-    if Penthouse.current_schema.nil?
-      "#{DEFAULT_SCHEMA_NAME}."
-    else
-      "#{Penthouse.current_schema}."
+    # override table name so it's calculated EVERY time
+    def self.table_name
+      table_name_prefix + original_table_name.gsub(/^.*\./, '')
+    end
+
+    # set a table name prefix which is dynamic based on the current schema
+    def self.table_name_prefix
+      if Penthouse.current_schema.nil?
+        "#{DEFAULT_SCHEMA_NAME}."
+      else
+        "#{Penthouse.current_schema}."
+      end
     end
   end
 end
