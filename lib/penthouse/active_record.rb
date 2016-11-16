7964059a4fab82bee02a6e19794fa4b7d5dc5599
Ryan Townsend <ryan@ryantownsend.co.uk>
2016-11-16 13:03:45 +0000

    Updated table_name override [3]

--- lib/penthouse/active_record.rb
+++ lib/penthouse/active_record.rb
@@ -8,12 +8,12 @@ class ActiveRecord::Base
     alias_method :original_table_name, :table_name
 
     # override table name so it's calculated EVERY time
-    def self.table_name
+    def table_name
       table_name_prefix + original_table_name.gsub(/^.*\./, '')
     end
 
     # set a table name prefix which is dynamic based on the current schema
-    def self.table_name_prefix
+    def table_name_prefix
       if Penthouse.current_schema.nil?
         "#{DEFAULT_SCHEMA_NAME}."
       else
